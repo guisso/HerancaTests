372ddf9b874f4cc9a474a40fd9a6c66d6177d078
guisso <lguisso@gmail.com>
2022-03-21 16:00:28 -0300

    Alteração da ordem dos testes: 1o herança e 2o polimorfismo.

--- src/main/java/io/github/guisso/herancatests/Programa.java
+++ src/main/java/io/github/guisso/herancatests/Programa.java
@@ -13,6 +13,38 @@ public class Programa {
     public static void main(String[] args) {
 
         //
+        // Testes com herança
+        //
+        
+        Pessoa p = new Pessoa();
+        p.setNome("Ana Zaira");
+        p.setEmail("ana@mail.com");
+
+        System.out.println(">> " + p);
+
+        Pessoa pf = new PessoaFisica();
+        pf.setNome("Beatriz Yana");
+        pf.setEmail("beatriz@mail.com");
+        pf.setTelefone(3821044141L);
+//        pf.setCpf(12345678901L);
+
+        // Qual toString() é invocado????
+        // O de Pessoa (o tipo da variável)?
+        // Ou o de PessoaFisica (o objeto referenciado)?
+        // O que se pode concluir?
+        System.out.println("?? " + pf);
+
+        PessoaFisica novaPf = (PessoaFisica) pf;
+        novaPf.setCpf(789456123L);
+
+        System.out.println(">> " + novaPf);
+
+        PessoaJuridica pj = new PessoaJuridica();
+        pj.setCnpj(1234567890123L);
+
+        System.out.println(">> " + pj);
+
+        //
         // Testes com herança e polimorfismo
         //
         
@@ -40,13 +72,13 @@ public class Programa {
         ec.setVendas(10000f);
         ec.setTaxa(.1f);
         empregados.add(ec);
-        
+
         // 90,00
         eh = new EmpregadoHorista();
         eh.setHoras(9f);
         eh.setValorHora(10f);
         empregados.add(eh);
-        
+
         // 811,00
         em = new EmpregadoMensalista();
         em.setSalario(811f);
@@ -59,37 +91,6 @@ public class Programa {
         System.out.println(">> Total da folha de pagamento: "
                 + "R$ " + salarioTotal);
 
-        //
-        // Testes com herança
-        //
-        
-        Pessoa p = new Pessoa();
-        p.setNome("Ana Zaira");
-        p.setEmail("ana@mail.com");
-        
-        System.out.println(">> " + p);
-        
-        Pessoa pf = new PessoaFisica();
-        pf.setNome("Beatriz Yana");
-        pf.setEmail("beatriz@mail.com");
-        pf.setTelefone(3821044141L);
-//        pf.setCpf(12345678901L);
-
-        // Qual toString() é invocado????
-        // O de Pessoa (o tipo da variável)?
-        // Ou o de PessoaFisica (o objeto referenciado)?
-        // O que se pode concluir?
-        System.out.println("?? " + pf);
-
-        PessoaFisica novaPf = (PessoaFisica) pf;
-        novaPf.setCpf(789456123L);
-        
-        System.out.println(">> " + novaPf);
-        
-        PessoaJuridica pj = new PessoaJuridica();
-        pj.setCnpj(1234567890123L);
-        
-        System.out.println(">> " + pj);
     }
 
 }
