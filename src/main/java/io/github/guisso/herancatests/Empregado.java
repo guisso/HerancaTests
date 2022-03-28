eb6570108bb59c30904bf71162ee5f9cee6020ba
guisso <lguisso@gmail.com>
2022-03-28 15:51:22 -0300

    Implementação de interface de comparação por nome ascendente.

--- src/main/java/io/github/guisso/herancatests/Empregado.java
+++ src/main/java/io/github/guisso/herancatests/Empregado.java
@@ -2,10 +2,13 @@ package io.github.guisso.herancatests;
 
 /**
  * Classe Empregado
+ *
  * @author Luis Guisso <luis dot guisso at ifnmg dot edu dot br>
  * @version 0.1, 21/03/2022
  */
-public class Empregado {
+public abstract class Empregado
+        implements Comparable<Empregado> {
+
     private String nome;
 
     public String getNome() {
@@ -15,8 +18,25 @@ public class Empregado {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    public Float calcularSalario() {
-        return 0.0f;
+
+    public abstract Float calcularSalario();
+
+    //<editor-fold defaultstate="collapsed" desc="toString/compareTo">
+    @Override
+    public String toString() {
+        return "Empregado{"
+                + "nome=" + nome
+                + ", salário=R$ " + calcularSalario()
+                + '}';
     }
+
+    @Override
+    public int compareTo(Empregado outro) {
+        // Ascendente
+        return getNome().compareTo(outro.getNome());
+        // Descendente
+//        return outro.getNome().compareTo(nome);
+    }
+    //</editor-fold>
+
 }
